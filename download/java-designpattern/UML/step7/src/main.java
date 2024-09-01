7ffe42771effdeb9cc3460582bfdcd4fc7dadcae
HAJI <143566329+haji8-thehaji@users.noreply.github.com>
2024-09-01 15:25:12 +0900

    Create main.java

---
+++ download/java-designpattern/UML/step7/src/main.java
@@ -0,0 +1,18 @@
+public class Car {
+	//...
+	private Engine engine;
+	private Radio radio;
+	
+	public Car() {
+		engine = new Engine();
+		radio = new Radio();
+	}
+}
+
+class Engine {
+	//...
+}
+
+class Radio {
+	//...
+}

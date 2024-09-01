6501328077712ff83e6836d00f3f8754187f53fa
HAJI <143566329+haji8-thehaji@users.noreply.github.com>
2024-09-01 15:45:03 +0900

    Add files via upload

---
+++ download/java-designpattern/1.Strategy Pattern/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        GameCharacter character = new GameCharacter();
+        character.attack();
+
+        character.setWeapon(new Knife());
+        character.attack();
+
+        character.setWeapon(new Sword());
+        character.attack();
+    }
+}

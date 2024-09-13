e711d25b386d1925ead0ed56e6f185ef1f79acac
Your Name <you@example.com>
2024-09-13 10:06:34 +0900

    Thread

--- download/explorer-java/ThreadTest/src/ThreadTest.java
+++ download/explorer-java/ThreadTest/src/ThreadTest.java
@@ -1,9 +1,8 @@
 public class ThreadTest {
     public static void main(String[] args) {
-        Runnable r1 = new MyThread("*");
-        Runnable r2 = new MyThread("!");
-        Runnable r3 = new MyThread("&");
-
+        Runnable r1 = new MyThread("*",10);
+        Runnable r2 = new MyThread("!",100);
+        Runnable r3 = new MyThread("&",1000);
         Thread t1 = new Thread(r1);
         Thread t2 = new Thread(r2);
         Thread t3 = new Thread(r3);
@@ -12,15 +11,19 @@ public class ThreadTest {
         t3.start();
         System.out.println("종료");
     }
-
     static class MyThread implements Runnable {
         private String string;
-        public MyThread(String s) {
+        private int sleepTime;
+        public MyThread(String s, int sleepTime) {
             this.string = s;
+            this.sleepTime = sleepTime;
         }
-
         @Override
         public void run() {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException ex) {
+            }
             System.out.println(string);
         }
     }

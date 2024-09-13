c55033cc12058f9688caa8de362cdc46d029b4c8
Your Name <you@example.com>
2024-09-13 10:14:06 +0900

    Thread

--- download/explorer-java/ThreadTest/src/ThreadTest.java
+++ download/explorer-java/ThreadTest/src/ThreadTest.java
@@ -1,8 +1,14 @@
+
+import java.util.ArrayList;
+
 public class ThreadTest {
     public static void main(String[] args) {
-        Runnable r1 = new MyThread("*",100);
-        Runnable r2 = new MyThread("!",1000);
-        Runnable r3 = new MyThread("&",10000);
+
+        ArrayList strList = new ArrayList<String>();
+        
+        Runnable r1 = new MyThread(strList, "*",100);
+        Runnable r2 = new MyThread(strList, "!",100);
+        Runnable r3 = new MyThread(strList, "&",100);
         Thread t1 = new Thread(r1);
         Thread t2 = new Thread(r2);
         Thread t3 = new Thread(r3);
@@ -14,7 +20,9 @@ public class ThreadTest {
     static class MyThread implements Runnable {
         private String string;
         private int sleepTime;
-        public MyThread(String s, int sleepTime) {
+        private ArrayList strList;
+        public MyThread(ArrayList strList, String s, int sleepTime) {
+            this.strList = strList;
             this.string = s;
             this.sleepTime = sleepTime;
         }
@@ -25,6 +33,9 @@ public class ThreadTest {
             } catch (InterruptedException ex) {
             }
             System.out.println(string);
+            strList.add(string);
+            System.out.println(strList);
+
         }
     }
 }

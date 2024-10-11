ebad1d6c65381d51aeb303d0a0ec6bb1bd6fa576
Your Name <you@example.com>
2024-10-11 13:31:24 +0900

    set member

--- download/java-spring-boot/agron_test/thymeleaf_test/src/main/java/com/haji/demo/thymeleaf_test/controller/TestController.java
+++ download/java-spring-boot/agron_test/thymeleaf_test/src/main/java/com/haji/demo/thymeleaf_test/controller/TestController.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.haji.demo.thymeleaf_test.model.Book;
+import com.haji.demo.thymeleaf_test.model.Lemp;
 import com.haji.demo.thymeleaf_test.service.BookService;
+import com.haji.demo.thymeleaf_test.service.LempService;
 
 @Controller
 public class TestController {
 
     @Autowired
 	private BookService bookService;
+    
+    @Autowired
+	private LempService lempService;
     
 	// @GetMapping("/test")
 	// public String index() {
@@ -26,6 +30,10 @@ public class TestController {
 	public String viewBookList(Model model) {
 		List<Book> books = bookService.getAllBooks();
 		model.addAttribute("allBooks", books);
+		
+		List<Lemp> lemps = lempService.getAllLemps();
+		model.addAttribute("allLemps", lemps);
+		
         return "index";
 	}
 

883ad16e790cc60e078b352a08db0abe1e7ea9f7
Your Name <you@example.com>
2024-11-01 10:59:33 +0900

    main

--- download/java-spring-boot/agron_test/thymeleaf_test/src/main/resources/schema.sql
+++ download/java-spring-boot/agron_test/thymeleaf_test/src/main/resources/schema.sql
@@ -2,6 +2,12 @@ DROP TABLE book2 IF EXISTS;
 DROP TABLE lemp IF EXISTS;
 DROP TABLE boardHaji IF EXISTS;
 DROP TABLE board IF EXISTS;
+DROP TABLE FOOD_INFO IF EXISTS;
+DROP TABLE MEMO IF EXISTS;
+DROP TABLE RECIPE IF EXISTS;
+DROP TABLE FOOD_INVENTORY IF EXISTS;
+DROP TABLE USER IF EXISTS;
+DROP TABLE CART IF EXISTS;
 
 CREATE TABLE book2 (
   book_id VARCHAR(5),
@@ -30,3 +36,53 @@ CREATE TABLE board (
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   views INT DEFAULT 0
 );
+
+CREATE TABLE `FOOD_INFO` (
+  `id` integer PRIMARY KEY,
+  `name` String,
+  `standard_expire_day` interger,
+  `standard_price` interger,
+  `standard_volume` string,
+  `storage_type` string,
+  `custom_bool` integer,
+  `season_spring` interger,
+  `season_summer` interger,
+  `season_autumn` interger,
+  `season_winter` interger
+);
+
+CREATE TABLE `MEMO` (
+  `id` integer PRIMARY KEY,
+  `memo` string COMMENT 'Content of the memo',
+  `created_dt` timestamp,
+  `deleted_dt` timestamp
+);
+
+CREATE TABLE `RECIPE` (
+  `id` integer PRIMARY KEY,
+  `Ingredients` string,
+  `Steps` string
+);
+
+CREATE TABLE `FOOD_INVENTORY` (
+  `id` integer PRIMARY KEY,
+  `food_id` integer,
+  `expire_date` timestamp,
+  `volume` string,
+  `created_dt` timestamp,
+  `deleted_dt` timestamp
+);
+
+CREATE TABLE `USER` (
+  `id` integer PRIMARY KEY
+);
+
+CREATE TABLE `CART` (
+  `id` integar PRIMARY KEY,
+  `food_id` integer,
+  `volume` string
+);
+
+ALTER TABLE `FOOD_INVENTORY` ADD FOREIGN KEY (`food_id`) REFERENCES `FOOD_INFO` (`id`);
+
+ALTER TABLE `CART` ADD FOREIGN KEY (`food_id`) REFERENCES `FOOD_INFO` (`id`);
